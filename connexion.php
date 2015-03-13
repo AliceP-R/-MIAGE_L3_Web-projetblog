03f872fe9784360e0f24325fd078e927697aa65a
AliceP-R <alice.pottier@free.fr>
2015-03-13 12:44:33 +0100

    Inscription et Connexion OK !

--- connexion.php
+++ connexion.php
@@ -1,15 +1,105 @@
-<p style="text-align:center">
-<h2>Bienvenue veuillez vous identifier.<br /></h2>
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+		<meta charset="utf-8" />
+		<TITLE> Inscription </TITLE>
+	</HEAD>
+	<BODY>
+	<form method="POST" action="">
+		<label for="login">Identifiant</label><br />
+		<input type="text" name="login" id="login"/><br />
+		<label for="password">Mot de Passe</label><br />
+		<input type="password" name="mdp" id="password"/><br /><br />
+		<input type="submit" name="Valider" value="Connexion" />
+	</form>
+	<label for="bouton_inscript">Vous n'êtes pas inscrit ? Cliquez ici !</label><br/>
+	<form method="POST" action="inscription.php">
+		<input type="submit" name="bouton_inscript" name="S'inscrire" value="Inscription" />
+	</form>
 
-<h3>
-<form method="POST" action="connexion.php">
-	<label for="login">Identifiant</label><br />
-	<input type="text" name="login" id="login"/><br />
-	<label for="password">Mot de Passe</label><br />
-	<input type="password" name="password" id="password"/><br /><br />
-	<input type="submit" name="Envoyer" value="Connexion" />
-</form>
-<form method="POST" action="inscription.php">
-	<input type="submit" name="S'inscrire" value="Inscription" />
-</form>
-</h3>
+
+	<?php
+		include("config.php");
+
+		function connexion($cid, $login, $mdp) 
+		{
+  			//Cryptage du mdp pour le comparer a celui de la BD
+  			$mdps=sha1($mdp);
+  
+  			//Début du SQL
+  			$query = "SELECT `Mdp` FROM `utilisateur` WHERE `login`=\"".$_POST['login']."\";";
+  			mysql_select_db("projet_blog");
+  			$res=mysql_query($query, $cid);
+  			//Fin du SQL
+  
+  			//Si la requete echoue
+  			if($res == FALSE) 
+    			echo "ERREUR de requete";
+  
+  			//Si la requete renvoie un résultat...   
+  			else
+  			{
+    			//..on le stock dans $arr...
+    			$arr=mysql_fetch_assoc($res);
+    
+			    //...et on le compare avec le mdp envoyé par l'utilisateur.
+    			if($arr['Mdp']==$mdps)
+      				return TRUE;
+      
+			    else
+      				return FALSE;
+  			}
+		}
+
+    	/*Si l'utilisateur clique sur valider*/
+    	if(isset($_POST['Valider']))
+    	{
+      		/*Connection a la base de données*/
+      		if(!($cid=mysql_connect("localhost", $user,$password)))
+      		{
+				die("Erreur de connexion à la base de données.<br/>");
+      		}
+  
+	    	//Si les champs ne sont pas vides...
+    		if(strcmp($_POST['login'], "") != 0 AND strcmp($_POST['mdp'], "") !=0)
+      		{
+				//...on les envois à la fonction connexion qui renvoi une valeur stockée dans $connex...
+				$connex=connexion($cid, $_POST['login'], $_POST['mdp']);
+	
+				//...si les identifiants sont corects...
+				if($connex==TRUE)
+				{
+	  				//...on enregistre le pseudo dans une variable de session et on envoi l'utilisateur sur sa page perso.
+	  				$_SESSION['pseudo']=$_POST['login'];
+	  				$_SESSION['test_connect']=1;
+	  				echo "Connexion ok"; 
+				}
+	
+				//...sinon on affiche un message d'erreur.
+				else
+				{
+	  				echo "</br><div class=\"alert alert-error span4\" >";
+	  				echo "Identifiants incorrects.";
+	  				echo "</div><br/></br>";
+				}
+      		}
+      
+      		//Si l'utilisateur n'a pas entré de login => Message d'erreur
+      		else if(strcmp($_POST['login'], "") == 0)
+      		{
+	  			echo "</br><div class=\"alert alert-error span4\" >";
+	  			echo "Entrez votre login.";
+	  			echo "</div><br/></br>";
+			}
+
+		    //Si l'utilisateur n'a pas entré de login => Message d'erreur
+      		else if(strcmp($_POST['mdp'], "") ==0)
+      		{
+	  			echo "</br><div class=\"alert alert-error span4\" >";
+	  			echo "Entrez votre mot de passe.";
+	  			echo "</div><br/></br>";
+			}
+    	}
+	?>
+	</BODY>
+</HTML>
--- inscription.php
+++ inscription.php
@@ -17,35 +17,47 @@
       		/*Si l'utilisateur clique sur Valider*/
       		if(isset($_POST['Valider']))
       		{
-      			if($_POST['mdp'] != $_POST['cmdp'])
+      			if(($_POST['Login'] == "") || ($_POST['mdp'] == "") || ($_POST['cmdp'] == ""))
 	  			{
-	  				die("Mot de passe différents"); 
+	  				die("Vous n'avez pas rempli tout les champs"); 
 	  			}
-				/*Connection a la base de donnée*/
-				if(!($cid=mysql_connect("localhost", $user,$password)))
-				{
-	  				die("erreur");
-				}
-				//cryptage du mot de passe entré par l'utilisateur
-  				$mdps=sha1($_POST['mdp']);
-  
-  				//Debut du SQL
-  				$requete = "INSERT INTO  `projet_blog`.`utilisateur`(`Login`, `Mdp`, `Droit`) 
-  							VALUES ('".$_POST['Login']."',  '".$mdps."',  'Lambda');"; 
+	  			else
+	  			{
+      				if($_POST['mdp'] != $_POST['cmdp'])
+	  				{
+	  					die("Mot de passe différents"); 
+	  				}
 
-  				mysql_select_db("test");
-  				$res=mysql_query($requete, $cid);
-  				//Fin du SQL  
-  				//Si l'insertion a échoué => Message d'erreur
-  				if($res == FALSE) 
-  				{
-  					echo "<br/>Inscriton ratée<br/>"; 
-  					echo "Ce login existe déjà"; 
-  				}
-  				//Si l'insertion est réussi => Message de succès
-  				else 
-    				echo "Inscription ok"; 
+					/*Connection a la base de donnée*/
+					if(!($cid=mysql_connect("localhost", $user,$password)))
+					{
+	  					die("Erreur à la connexion de la base de données");
+					}
+					//cryptage du mot de passe entré par l'utilisateur
+  					$mdps=sha1($_POST['mdp']);
+  
+  					//Debut du SQL
+  					$requete = "INSERT INTO  `projet_blog`.`utilisateur`(`Login`, `Mdp`, `Droit`) 
+  								VALUES ('".$_POST['Login']."',  '".$mdps."',  'Lambda');"; 
+
+  					mysql_select_db("test");
+  					$res=mysql_query($requete, $cid);
+  					//Fin du SQL  
+  					//Si l'insertion a échoué => Message d'erreur
+  					if($res == FALSE) 
+  					{
+  						echo "<br/>Inscriton ratée<br/>"; 
+  						echo "Ce login existe déjà<br/>"; 
+  					}
+  					//Si l'insertion est réussi => Message de succès
+  					else 
+    					echo "Inscription ok<br/>"; 
+    			}
 			}
 	  	?>
+	  	<label for="bouton_connexion">Déjà inscrit ? Cliquez ici !</label><br/>
+	  	<form method="POST" action="connexion.php">
+			<input type="submit" name="bouton_connexion" name="Se connecter" value="Connexion" />
+		</form>
 	</BODY>
 </HTML>
