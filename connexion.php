15f4003f4b7a2e166791ea5ae1961ed45d2721c0
Hemeris <menguy.philippe@laposte.net>
2015-03-13 10:36:03 +0100

    Maj Connexion

--- connexion.php
+++ connexion.php
@@ -2,11 +2,14 @@
 <h2>Bienvenue veuillez vous identifier.<br /></h2>
 
 <h3>
-<form method="POST" action="index.php?page=connexion">
+<form method="POST" action="connexion.php">
 	<label for="login">Identifiant</label><br />
 	<input type="text" name="login" id="login"/><br />
 	<label for="password">Mot de Passe</label><br />
 	<input type="password" name="password" id="password"/><br /><br />
 	<input type="submit" name="Envoyer" value="Connexion" />
+</form>
+<form method="POST" action="inscription.php">
+	<input type="submit" name="S'inscrire" value="Inscription" />
 </form>
 </h3>
